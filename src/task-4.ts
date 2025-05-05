446e2eb5fc4f8104c6f01928cb9df8713533688c
Oleksii Ivaniuta <11.ivaniuta@gmail.com>
2025-05-05 19:45:13 +0100

    hw1 ready

--- src/task-4.ts
+++ src/task-4.ts
@@ -1,1 +1,10 @@
-console.log(`task4`);
+function printUserInfo(name: string, age: number, email?: string): void {
+  console.log("Name:", name);
+  console.log("Age:", age);
+  if (email) {
+    console.log("Email:", email);
+  }
+}
+
+printUserInfo("Alice", 30);
+printUserInfo("Bob", 25, "bob@mail.com");
